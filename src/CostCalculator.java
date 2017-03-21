c1911a7a59527c94a253e48f0d8e1bbaf212fe5a
Aseel Al Dallal <aseelaldallal@gmail.com>
2017-03-21 20:05:19 +0000

    read user input

--- src/CostCalculator.java
+++ src/CostCalculator.java
@@ -1,11 +1,21 @@
 
 public class CostCalculator {
     
+    private double basePrice;
+    private int numLabor;
+    private String materials;
+    
+    
     // Constructor
-    public CostCalculator() {
+    public CostCalculator(String basePrice, String numLabor, String materials) {
         
        
     }
     
+    
+    // Calculate
+    public String calculate() {
+        
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	       
+	      
+	      Scanner scanner = new Scanner(System.in);
+	      
+	      // Base Price
+          System.out.print("Enter Base Price: ");
+          String basePrice = scanner.nextLine();
+          
+          // Number of Laborers
+          System.out.print("Number of Laborers: ");
+          String numLabor = scanner.nextLine();
+          
+          // Materials
+          System.out.print("Type in the type of materials involved. Seperate each with a comma: ");
+          String materials = scanner.nextLine();
 
+          scanner.close();
+          
+          CostCalculator costCalc = new CostCalculator(basePrice, numLabor, materials);
+          System.out.println("Cost: " + costCalc.calculate());
+          
     }
     
     
